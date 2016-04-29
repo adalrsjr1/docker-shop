71dfafcc70006c685ab85fd6513e685a9ad12c5d
Adalberto R. Sampaio Junior <adalrsjr1@gmail.com>
2016-04-29 14:55:32 -0300

    test updated

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for i in {1..100}; do
-	curl -i -X GET "10.0.0.35:8100/match/public/login/Usuario$(echo $(($RANDOM % 10 + 1)) )";
-done
+run() {
+	for ((i=1; i<=$1; i++));do
+		# curl -i -X GET "10.0.0.35:8100/match/public/login/Usuario$(echo $(($RANDOM % 10 + 1)) )";
+		curl -i -X GET "192.168.201.254:8110/frontend/public/products?user=Usuario$( echo $(( $RANDOM % 10 + 1  ))  )";
+	done
+}
 
+if [ $1 = "run"  ]; then
+	run $2
+fi
