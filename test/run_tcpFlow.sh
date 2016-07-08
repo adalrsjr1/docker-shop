f946f06d7ee2d58710993ccb8fbf1411e44b9248
Adalberto R. Sampaio Junior <adalrsjr1@gmail.com>
2016-07-08 01:10:03 -0300

    tcpflow script updated

--- test/run_tcpFlow.sh
+++ test/run_tcpFlow.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo tcpflow -Ft -c -i docker0 | grep --line-buffered -oE '.* (GET|PUT|DELETE|POST|HEAD) .* HTTP/1.[01]|.* HTTP/1.[01] [0-9]+ [A-Z ]*' > /home/adalrsjr1/system.trace
+# sudo tcpflow -Ft -c -i docker0 | grep --line-buffered -oE '.* (GET|PUT|DELETE|POST|HEAD) .* HTTP/1.[01]|.* HTTP/1.[01] [0-9]+ [A-Z ]*' > /home/adalrsjr1/system.trace
+
+sudo tcpflow -Ft -c -i docker0 | awk '{if($0 ~ /.* (GET|PUT|DELETE|POST|HEAD) .* HTTP\/1.[01]|.* HTTP\/1.[01] [0-9]+ [A-Z ]*/) print ### $0; if(bash ~ /X-Unique-Id: *[a-z0-9]{32}/) print $0; fflush(stdout);}' > /home/adalrsjr1/system.trace2
